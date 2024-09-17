de80c77253d788e958c8df870844dfa1c3a9c076
Raul Gabriel <raul.stoianof@cirvianum.cat>
2024-09-17 11:25:51 +0200

    Creació de index.php i docker-compose.yaml

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple PHP amb Docker</title>
+</head>
+<body>
+    <h1>Benvingut a la pàgina amb PHP!</h1>
+    <p>Aquesta és una aplicació PHP desplegada amb Docker.</p>
+</body>
+</html>
